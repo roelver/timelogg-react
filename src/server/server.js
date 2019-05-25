326b8a3c56272d262638291cf0b1fa44a861ea55
roelver <roel@romaniflo.nl>
2019-05-25 22:07:41 +0200

    Heroku backend

--- src/server/server.js
+++ src/server/server.js
@@ -23,18 +23,14 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
-//production mode
+//production mode: host React app from here
 if (process.env.NODE_ENV === 'production') {
-    console.log(__dirname);
-    fs.readdir('.', function(err, items) {
-        console.log(items);
-    });
     app.use(express.static('build'));
-    app.use('/roelver/timelogg-react/static/js', express.static('build/static/js'));
-    app.use('/roelver/timelogg-react/static/css', express.static('build/static/css'));
-    app.get('/', (req, res) => {
-        res.sendFile('build/index.html');
-    })
+    app.use('/static/js', express.static('build/static/js'));
+    app.use('/static/css', express.static('build/static/css'));
+    // app.get('/', (req, res) => {
+    //     res.sendFile('build/index.html');
+    // })
 }
 
 app.use(express.json());
