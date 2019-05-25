61af4983e08e009e180cf1732ed4a1e03c013758
roelver <roel@romaniflo.nl>
2019-05-25 21:21:26 +0200

    Heroku backend

--- src/server/server.js
+++ src/server/server.js
@@ -24,12 +24,12 @@ app.use((req, res, next) => {
 });
 //production mode
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'build')));
-    //
+    app.use('/roelver/timelogg-react/static/js', express.static(path.join(__dirname, 'build/static/js')));
+    app.use('/roelver/timelogg-react/static/css', express.static(path.join(__dirname, 'build/static/css')));
     app.get('*', (req, res) => {
-      res.sendfile(path.join(__dirname = 'build/index.html'));
-    })
-  }
+        res.sendfile(path.join(__dirname, 'build/index.html'));
+      })
+    }
 
 app.use(express.json());
 app.use(userRouter);
